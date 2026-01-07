87fa55a491dcf624fba7728e45bb353224ea8697
Oleh Solianik <olehsolianik@gmail.com>
2026-01-07 19:57:17 +0200

    feat(ui): render pages and handle create page action

--- src/js/app/events.js
+++ src/js/app/events.js
@@ -1,3 +1,23 @@
-export function bindEvents() {
-  console.log('Events binded');
+// =========================
+// Imports
+// =========================
+
+import { createPage } from "./state.js";
+import { saveStateToStorage } from "../services/storage.js";
+import { renderApp } from "./render.js";
+
+// =========================
+// Application logic
+// =========================
+
+function bindEvents() {
+  const createPageBtn = document.querySelector('[data-action="create-page"]');
+
+  createPageBtn.addEventListener('click', ()=> {
+    createPage();
+    saveStateToStorage();
+    renderApp();
+  });
 }
+
+export { bindEvents };
--- src/js/app/render.js
+++ src/js/app/render.js
@@ -1,3 +1,37 @@
-export function renderApp() {
-  console.log('App rendered');
+// =========================
+// Imports
+// =========================
+
+import { getState } from "./state.js";
+
+// =========================
+// Application logic
+// =========================
+
+function renderApp() {
+  renderSidebar();
+  renderEditor();
 }
+
+function renderSidebar() {
+  const { pages, activePageId } = getState();
+  const sidebarNav = document.querySelector('.sidebar__nav');
+
+  sidebarNav.innerHTML = pages.map(page => 
+    `
+    <div class="nav__page ${page.id === activePageId ? 'nav__page--active' : ''}" data-id="${page.id}">
+      ${page.title}
+    </div>
+    `
+  ).join('');
+}
+
+function renderEditor() {
+  const { pages, activePageId } = getState();
+  const editorContent = document.querySelector('.editor__content');
+
+  const activePage = pages.find(page => page.id === activePageId);
+  editorContent.innerHTML = activePage ? `<h1>${activePage.title}</h1>` : '';
+}
+
+export { renderApp };
