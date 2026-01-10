f573de8b8aaa21c0087a6f84b621ac0a720b8943
Oleh Solianik <olehsolianik@gmail.com>
2026-01-10 19:36:33 +0200

    feat(render): add breadcrumbs UI to editor header

--- src/js/components/editor/renderEditor.js
+++ src/js/components/editor/renderEditor.js
@@ -2,13 +2,37 @@
 // Imports
 // =========================
 
-import { getState } from "../../app/state.js";
+import { getState, getPagePath } from "../../app/state.js";
 
 // =========================
 // Application logic
 // =========================
 
 function renderEditor() {
+  renderEditorHeader();
+  renderEditorContent();
+}
+
+function renderEditorHeader() {
+  const editorHeader = document.querySelector('.editor__header');
+  const { activePageId } = getState();
+
+  if(!activePageId) {
+    editorHeader.innerHTML = '';
+    return;
+  }
+
+  const pagePath = getPagePath(activePageId);
+  editorHeader.innerHTML = pagePath.map((page, index) => 
+  `
+  <span class="breadcrumb__item" data-page-id="${page.id}">
+    ${page.title}
+  <span>
+  ${index < pagePath.length - 1 ? '/' : ''}
+  `).join('');
+}
+
+function renderEditorContent() {
   const { pages, activePageId } = getState();
   const editorContent = document.querySelector('.editor__content');
 
